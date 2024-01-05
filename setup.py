cfd68e6429b38e3d20d5b6438aab3c4fae17de19
Eugene Gubenkov <gubenkoved@gmail.com>
2024-01-05 23:40:18 +0100

    setup.py changes

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_namespace_packages
 
 
 if __name__ == '__main__':
+    with open('requirements.txt') as f:
+        requirements = [
+            line for line in f.readlines()
+            if not line.strip().startswith('#')
+        ]
+
     setup(
         name='sync',
         version='0.0.1',
         packages=find_namespace_packages(where='src'),
         package_dir={'': 'src'},
-        install_requires=[
-        ],
+        install_requires=requirements,
     )
