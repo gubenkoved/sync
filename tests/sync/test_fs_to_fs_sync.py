c0e23e0d70bf61a8b2c913aebaf8404ec96db614
Eugene Gubenkov <gubenkoved@gmail.com>
2024-06-01 00:11:57 +0200

    added some basic tests for sync

--- tests/sync/test_fs_to_fs_sync.py
+++ tests/sync/test_fs_to_fs_sync.py
@@ -1,5 +1,8 @@
-from sync.core import Syncer
+import shutil
+import tempfile
 
+from sync.core import Syncer
+from sync.providers.fs import FSProvider
 from tests.sync.test_sync import SyncTestBase
 
 
@@ -7,4 +10,16 @@ class FsToFsSyncTest(SyncTestBase):
     __test__ = True
 
     def get_syncer(self) -> Syncer:
-        raise NotImplementedError
+        src_dir = tempfile.mkdtemp()
+        dst_dir = tempfile.mkdtemp()
+
+        src_provider = FSProvider(root_dir=src_dir)
+        dst_provider = FSProvider(root_dir=dst_dir)
+
+        self.addCleanup(lambda: shutil.rmtree(src_dir))
+        self.addCleanup(lambda: shutil.rmtree(dst_dir))
+
+        return Syncer(
+            src_provider,
+            dst_provider,
+        )
--- tests/sync/test_sync.py
+++ tests/sync/test_sync.py
@@ -3,8 +3,10 @@ from unittest import TestCase
 
 import pytest
 from sync.core import Syncer
+from tests.common import bytes_as_stream, stream_to_bytes
 
 
+# TODO: update tests to check for sync actions
 class SyncTestBase(TestCase):
     __test__ = False
 
@@ -17,21 +19,108 @@ class SyncTestBase(TestCase):
         src_provider = syncer.src_provider
         dst_provider = syncer.dst_provider
 
+        syncer.sync()
 
-        src_provider.write('foo')
-        raise NotImplementedError
+        self.assertEqual(0, len(dst_provider.get_state().files))
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('foo', stream)
+
+        syncer.sync()
+
+        self.assertEqual(1, len(dst_provider.get_state().files))
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('bar', stream)
+
+        syncer.sync()
+
+        self.assertEqual(2, len(dst_provider.get_state().files))
 
     def test_sync_updated_files(self):
-        raise NotImplementedError
+        syncer = self.get_syncer()
+        src_provider = syncer.src_provider
+        dst_provider = syncer.dst_provider
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('foo', stream)
+
+        syncer.sync()
+
+        self.assertEqual(
+            b'data',
+            stream_to_bytes(dst_provider.read('foo'))
+        )
+
+        with bytes_as_stream(b'updated') as stream:
+            src_provider.write('foo', stream)
+
+        syncer.sync()
+
+        self.assertEqual(
+            b'updated',
+            stream_to_bytes(dst_provider.read('foo'))
+        )
 
     def test_sync_deleted_files(self):
-        raise NotImplementedError
+        syncer = self.get_syncer()
+        src_provider = syncer.src_provider
+        dst_provider = syncer.dst_provider
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('foo', stream)
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('bar', stream)
+
+        syncer.sync()
+
+        self.assertEqual(2, len(dst_provider.get_state().files))
+
+        src_provider.remove('foo')
+
+        syncer.sync()
+
+        self.assertEqual(1, len(dst_provider.get_state().files))
+
+        src_provider.remove('bar')
+
+        syncer.sync()
+
+        self.assertEqual(0, len(dst_provider.get_state().files))
 
     def test_sync_moved_files(self):
-        raise NotImplementedError
+        syncer = self.get_syncer()
+        src_provider = syncer.src_provider
+        dst_provider = syncer.dst_provider
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('foo', stream)
+
+        syncer.sync()
+
+        self.assertEqual(1, len(dst_provider.get_state().files))
+
+        # move the file
+        src_provider.move('foo', 'bar')
+
+        syncer.sync()
+
+        self.assertEqual(1, len(dst_provider.get_state().files))
 
     def test_conflicting_updates(self):
-        raise NotImplementedError
+        syncer = self.get_syncer()
+        src_provider = syncer.src_provider
+        dst_provider = syncer.dst_provider
+
+        with bytes_as_stream(b'data') as stream:
+            src_provider.write('foo', stream)
+
+        with bytes_as_stream(b'data') as stream:
+            dst_provider.write('foo', stream)
+
+        # no errors
+        syncer.sync()
 
 
 if __name__ == '__main__':
