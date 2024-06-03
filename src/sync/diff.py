21f025945078485eab901978ec08510c9c37ff53
Eugene Gubenkov <gubenkoved@gmail.com>
2024-06-04 01:27:47 +0200

    use levenstein distance when detecting movement using best match

--- src/sync/diff.py
+++ src/sync/diff.py
@@ -44,6 +44,23 @@ class MovedDiffType(DiffType):
             self.__class__.__name__, self.path, self.new_path)
 
 
+# https://stackoverflow.com/questions/2460177/edit-distance-in-python
+def levenshtein_distance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+
 class StorageStateDiff:
     def __init__(self, changes: Dict[str, DiffType]):
         self.changes: Dict[str, DiffType] = changes
@@ -85,12 +102,11 @@ class StorageStateDiff:
                 def score(added_diff: AddedDiffType, removed_diff: RemovedDiffType):
                     _, added_filename = path_split(added_diff.path)
                     _, removed_filename = path_split(removed_diff.path)
-                    # TODO: use edit distance instead!
-                    return sum(1 for _ in set(added_filename) & set(removed_filename))
+                    return levenshtein_distance(added_filename, removed_filename)
 
                 for removed_diff in removed_diffs:
                     # pick added diff with best score
-                    best_match_added_diff = max(
+                    best_match_added_diff = min(
                         added_diffs, key=lambda added_diff: score(added_diff, removed_diff))
                     added_diffs.remove(best_match_added_diff)
 
