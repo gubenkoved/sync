fcebf044b422901270f2a9c9691065ed4c69e66c
Eugene Gubenkov <gubenkoved@gmail.com>
2024-06-18 15:08:11 +0200

    use requirements.in in package definition

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 
 if __name__ == '__main__':
-    with open('requirements.txt') as f:
+    with open('requirements.in') as f:
         requirements = [
             line for line in f.readlines()
             if not line.strip().startswith('#')
