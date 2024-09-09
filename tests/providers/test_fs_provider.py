5b787ba35c4ac52a0c58f746eb3f4884927e0ab3
Eugene Gubenkov <gubenkoved@gmail.com>
2024-09-09 22:50:45 +0200

    cache based test for multiple files

--- tests/providers/test_fs_provider.py
+++ tests/providers/test_fs_provider.py
@@ -4,6 +4,7 @@ import tempfile
 import unittest
 import unittest.mock as mock
 import logging
+from importlib.metadata import pass_none
 
 from sync.cache import InMemoryCache
 from sync.core import ProviderBase
@@ -27,6 +28,17 @@ class FSProviderTest(ProviderTestBase):
 
     def get_provider(self) -> ProviderBase:
         return self.provider
+
+    @staticmethod
+    def ensure_modification_time_changed(path, previous_mtime):
+        while True:
+            os.utime(path)
+            current_time = os.path.getmtime(path)
+            if current_time != previous_mtime:
+                break
+            LOGGER.debug(
+                'repeat changing modification time as it did'
+                'not seem to change...')
 
     def test_file_hash_is_taken_from_cache_until_file_modified(self):
         with mock.patch('sync.providers.fs.sha256_stream') as patcher:
@@ -55,11 +67,7 @@ class FSProviderTest(ProviderTestBase):
             patcher.reset_mock()
 
             # change modification date
-            os.utime(foo_path)
-
-            mtime2 = os.path.getmtime(foo_path)
-            LOGGER.info('mtime2: %s', mtime)
-            self.assertNotEqual(mtime, mtime2)
+            self.ensure_modification_time_changed(foo_path, mtime)
 
             # cache miss
             _ = self.provider.get_state()
@@ -71,6 +79,57 @@ class FSProviderTest(ProviderTestBase):
 
             _ = self.provider.get_state()
             self.assertEqual(1, patcher.call_count)
+            patcher.reset_mock()
+
+    def test_file_hash_is_taken_from_cache_until_file_modified_multiple_files(self):
+        with mock.patch('sync.providers.fs.sha256_stream') as patcher:
+            patcher.return_value = 'test_hash'
+
+            with bytes_as_stream(b'foo') as stream:
+                self.provider.write('foo', stream)
+
+            with bytes_as_stream(b'bar') as stream:
+                self.provider.write('bar', stream)
+
+            # cache miss
+            _ = self.provider.get_state()
+            self.assertEqual(2, patcher.call_count)
+            patcher.reset_mock()
+
+            # cache hit
+            _ = self.provider.get_state()
+            self.assertEqual(0, patcher.call_count)
+            patcher.reset_mock()
+
+            foo_path = os.path.join(self.root_dir, 'foo')
+            bar_path = os.path.join(self.root_dir, 'bar')
+
+            # modify one
+            foo_mtime = os.path.getmtime(foo_path)
+            self.ensure_modification_time_changed(foo_path, foo_mtime)
+
+            # cache miss for one
+            _ = self.provider.get_state()
+            self.assertEqual(1, patcher.call_count)
+            patcher.reset_mock()
+
+            # modify both
+            foo_mtime = os.path.getmtime(foo_path)
+            bar_mtime = os.path.getmtime(foo_path)
+            self.ensure_modification_time_changed(foo_path, foo_mtime)
+            self.ensure_modification_time_changed(bar_path, bar_mtime)
+
+            # cache miss for both
+            _ = self.provider.get_state()
+            self.assertEqual(2, patcher.call_count)
+            patcher.reset_mock()
+
+            # clear cache
+            self.cache.clear()
+
+            # cache miss for both
+            _ = self.provider.get_state()
+            self.assertEqual(2, patcher.call_count)
             patcher.reset_mock()
 
 
